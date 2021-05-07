268e8ca5ef09f54417f5294317c0f1e3a6b83973
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-07 12:27:42 +0200

    adds post reviews

--- src/services/reviews.js
+++ src/services/reviews.js
@@ -9,6 +9,7 @@ router.post('/:productId/:userId', async (req, res, next) => {
       productId: req.params.productId,
       userId: req.params.userId,
     });
+    res.send(rev);
   } catch (error) {
     console.log(error);
   }
